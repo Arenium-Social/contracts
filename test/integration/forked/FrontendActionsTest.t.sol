fd4f7a94aa8763b1bc71b0ef5a944fb6dcd127bb
Shawn Rizo <shawnrizo@hotmail.com>
2025-02-04 18:21:40 -0500

    Resolving Branch issues

---
+++ test/integration/forked/FrontendActionsTest.t.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.16;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {HelperConfig} from "../../../script/HelperConfig.s.sol";
+import {PredictionMarket} from "../../../src/PredictionMarket.sol";
+import {UniswapV3AMMContract} from "../../../src/UniswapV3AMMContract.sol";
+import {StdUtils} from "../../../lib/forge-std/src/StdUtils.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract FrontendActionsTest is Test {
+    HelperConfig helpConfig = new HelperConfig();
+    HelperConfig.NetworkConfig config;
+    UniswapV3AMMContract amm;
+    PredictionMarket market;
+
+    address owner = address(1);
+    address user = address(2);
+    uint256 baseSepoliaFork;
+    string BASE_SEPOLIA_RPC_URL = vm.envString("BASE_SEPOLIA_RPC_URL");
+
+    function setUp() public {
+        config = helpConfig.getBaseSepoliaConfig();
+        baseSepoliaFork = vm.createSelectFork(BASE_SEPOLIA_RPC_URL);
+        deal(config.currency, user, 500000000);
+        vm.startPrank(owner);
+        amm = new UniswapV3AMMContract(config.uniswapV3Factory, config.uniswapV3SwapRouter);
+        market = new PredictionMarket(config.finder, config.currency, config.optimisticOracleV3, address(amm));
+        vm.stopPrank();
+    }
+
+    // function testFunction() public {
+    //     vm.startPrank(user);
+    //     IERC20(config.currency).approve(address(market), 500000000);
+    //     bytes32 marketId = market.initializeMarket(
+    //         "yes",
+    //         "no",
+    //         "test market",
+    //         500000000, // 500 USDC reward
+    //         100000000, // 100 USDC bond
+    //         3000
+    //     );
+    //     vm.stopPrank();
+    // }
+}
