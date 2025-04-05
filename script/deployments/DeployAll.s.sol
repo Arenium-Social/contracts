0f37107d98394cca28f9e6053a3204167bacd0cc
Shawn Rizo <shawnrizo@hotmail.com>
2025-04-05 15:04:13 -0400

    Edit: Refactor DeployAll Script

--- script/deployments/DeployAll.s.sol
+++ script/deployments/DeployAll.s.sol
@@ -2,12 +2,13 @@
 pragma solidity 0.8.16;
 
 import {Script, console2} from "forge-std/Script.sol";
+import {PredictionMarketManager} from "../../src/PredictionMarketManager.sol";
 import {PredictionMarket} from "../../src/PredictionMarket.sol";
 import {AMMContract} from "../../src/AMMContract.sol";
 import {HelperConfig} from "../HelperConfig.s.sol";
 
 contract DeployAll is Script {
-    function run() external returns (PredictionMarket) {
+    function run() external {
         HelperConfig helpConfig = new HelperConfig();
         HelperConfig.NetworkConfig memory config;
 
@@ -17,11 +18,12 @@ contract DeployAll is Script {
         AMMContract amm = new AMMContract(
             config.uniswapV3Factory, config.uniswapV3SwapRouter, config.uniswapNonFungiblePositionManager
         );
+        PredictionMarketManager marketManager = new PredictionMarketManager();
         PredictionMarket market =
             new PredictionMarket(config.finder, config.currency, config.optimisticOracleV3, address(amm));
         vm.stopBroadcast();
-        console2.log("PredictionMarket deployed to: ", address(market));
         console2.log("AMM deployed to: ", address(amm));
-        return market;
+        console2.log("PredictionMarketManager deployed to: ", address(marketManager));
+        console2.log("PredictionMarket deployed to: ", address(market));
     }
 }
