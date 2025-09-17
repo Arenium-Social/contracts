b68d187292ba13642ba0bc6022cc7d2d64d44766
Shawn Rizo <shawnrizo@hotmail.com>
2025-09-17 14:36:22 -0400

    feat: Add Contract Title Natspec Comments

--- src/onchain/TestPredictionMarketManager.sol
+++ src/onchain/TestPredictionMarketManager.sol
@@ -1,4 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.16;
 
+/**
+ * @title Test_PredictionMarketManager
+ * @notice Simplified version for testing whitelist functionality
+ */
 contract TestPredictionMarketManager {}
